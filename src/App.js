4ee0da2ccf36ef0180a74a09872328f4ffc7074c
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-05-12 20:47:45 +0000

    complete challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then(res => setRepositories(res.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post("/repositories", {
+      title: `Repositório ${Date.now()}`,
+      owner: "Bruno Pinheiro",
+    });
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repos = repositories.filter(repo => repo.id !== id);
+    await api.delete(`/repositories/${id}`);
+    setRepositories(repos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
